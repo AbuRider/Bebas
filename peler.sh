bb78b4db4d03311d48c3e7050fd08c4f9ecab396
AbuRider <dinarysf276@gmail.com>
2025-06-29 22:29:08 +0700

    Update peler.sh

--- peler.sh
+++ peler.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-subject='/C=US/ST=State/L=City/O=Android/OU=Android/CN=Android/emailAddress=email@example.com'
+subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
 for x in releasekey platform shared media networkstack verity otakey testkey sdk_sandbox bluetooth nfc; do \
     ./development/tools/make_key vendor/genesis/signing/keys/$x "$subject"; \
 done
